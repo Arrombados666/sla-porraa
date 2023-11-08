ae58ceaf1dac7e87952fa98e209f1c13b861c78e
Arrombados666 <149962457+Arrombados666@users.noreply.github.com>
2023-11-07 21:42:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    const accordionItems = document.querySelectorAll(".accordion-item");
+
+    accordionItems.forEach(function (item) {
+        const title = item.querySelector(".accordion-title");
+        const content = item.querySelector(".accordion-content");
+
+        title.addEventListener("click", function () {
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    });
+});
+
+
+
+function redirecionar(url) {
+    window.location.href = url;
+}
